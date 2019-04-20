af3707cbc3885674687d16faca57b6a98c89ffdb
Won Kim <wonsae.kim@gmail.com>
2019-04-20 19:22:44 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,14 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    res = []
+    self.each do |k, v|
+      arguments.each do |i|
+        if i == v
+          res << k
+        end
+      end
+    end
+    res
   end
 end
